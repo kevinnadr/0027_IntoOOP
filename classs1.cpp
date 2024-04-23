2a72aff770abe90ee05a598b49fac935ff9f2b94
kevinnadr <kevin19305.ib@gmail.com>
2024-04-23 19:13:20 +0700

    void display

--- classs1.cpp
+++ classs1.cpp
@@ -20,4 +20,10 @@ class bangundatar{
             return panjang * lebar;
         }
 
+        void display() {
+            cout << "Panjangnya = " << panjang << endl;
+            cout << "Lebarnya = " << lebar << endl;
+            cout << "Luasnya = " << hitungluas() << endl;
+        }
+
 }
